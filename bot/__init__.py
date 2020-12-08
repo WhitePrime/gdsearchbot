9c5be20bc1b3019f9f0ed37aee19d50d84e49d08
Madhavreddy672 <71065430+Madhavreddy672@users.noreply.github.com>
2020-12-08 23:58:13 +0530

    Update __init__.py
    Ok

--- bot/__init__.py
+++ bot/__init__.py
@@ -58,7 +58,7 @@ if os.path.exists('drive_folder'):
         lines = f.readlines()
         for line in lines:
             temp = line.strip().split()
-            DRIVE_NAME.append(temp[0].replace(" ", " "))
+            DRIVE_NAME.append(temp[0])
             DRIVE_ID.append(temp[1])
             try:
                 INDEX_URL.append(temp[2])
